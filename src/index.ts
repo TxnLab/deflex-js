d6c3ea8fa0eb831a4c21970bff5b227a4b3f0132
Doug Richar <drichar@gmail.com>
2025-10-26 15:17:20 -0400

    refactor: simplify exports with export * pattern

--- src/index.ts
+++ src/index.ts
@@ -1,44 +1,5 @@
-// Main client
-export { DeflexClient } from './client'
-
-// Swap composer
-export { SwapComposer } from './composer'
-
-// Client types
-export type {
-  DeflexConfigParams,
-  GetQuoteParams,
-  FetchSwapParams,
-  QuoteType,
-} from './types'
-
-// Composer types
-export type {
-  SignerFunction,
-  SwapComposerConfig,
-  SwapComposerStatus,
-  TransactionSigner,
-} from './composer'
-
-// API types
-export type {
-  QuoteResponse,
-  Asset,
-  Profit,
-  PathElement,
-  Route,
-  DexQuote,
-  TxnPayload,
-  SwapTxn,
-  SwapTxnsResponse,
-  SwapTxnSignature,
-} from './types'
-
-// Swap processing types
-export type { ProcessedTransaction } from './types'
-
-// Constants
-export { Protocol } from './constants'
-
-// Utilities
-export { HTTPError } from './request'
+export * from './client'
+export * from './composer'
+export * from './constants'
+export * from './request'
+export * from './types'
